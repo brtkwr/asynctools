4c44b45a176f54b1363c5e0ae4ad5f7cdd89f2d9
Bharat Kunwar <b.kunwar@gmail.com>
2018-04-15 13:19:10 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Description
+
+Downloads list of image URLs in a file asynchronously using Python3
+
+# Installation
+
+After cloning,
+
+  cd async_download
+  virtualenv -p python3 .
+  pip install -U pip
+  pip install -r requirements.txt
+
+# Usage
+
+  ./async_download.py urls.txt
+  ./async_download.py urls.txt <begin>
+  ./async_download.py urls.txt <begin> <end>
---
+++ async_download.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from aiohttp import ClientSession
+import asyncio
+import sys
+
+if len(sys.argv)>1:
+    URLS_FILE = sys.argv[1]
+else:
+    print('Usage: ./async.download.py <URLS FILE> <OPTIONAL:start_index> <OPTIONAL:end_index>')
+    sys.exit(1)
+
+with open(URLS_FILE) as f:
+    URLS = f.readlines()
+
+START = 0
+END = len(URLS)
+
+if len(sys.argv)>2:
+    START = int(sys.argv[2])
+
+if len(sys.argv)>3:
+    END = int(sys.argv[3])
+
+ZFILL = len(str(END))
+
+EXT = { 
+        'image/png': 'png',
+        'image/jpeg': 'jpeg',
+        'image/gif': 'gif',
+    }
+
+async def download(url, session, index):
+    try:
+        async with session.get(url) as response:
+            content = await response.read()
+            ctype = response.headers.get('content-type')
+            filename = '{}.{}'.format(str(index).zfill(ZFILL), EXT[ctype])
+            with open(filename, 'wb') as f:
+                f.write(content)
+                print(index, ctype)
+            return ctype
+    except Exception as e:
+        exception = type(e).__name__
+        print(index, exception)
+        return exception
+ 
+async def run():
+    async with ClientSession() as session:
+        tasks = [asyncio.ensure_future(download(URL, session, index + START)) for index, URL in enumerate(URLS[START:END])]
+        return await asyncio.gather(*tasks)
+ 
+def get_images():
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(run())
+    return loop.run_until_complete(future)
+ 
+responses = get_images()
+
+with open('response.log', 'w') as f:
+    for response in responses:
+        f.write('{}\n'.format(response))
